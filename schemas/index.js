fd8e719596b03d0d1fcd5e7dc4a89399e7754711
오윤지 <network_dude@naver.com>
2022-11-17 22:12:46 +0900

    .

--- schemas/index.js
+++ schemas/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   await mongoose.connect(
-    `mongodb+srv://hello:hello123@cluster0.0k542js.mongodb.net/?retryWrites=true&w=majority`,
+    ``,
     { dbName : "ASOproject" }
     );
 };
