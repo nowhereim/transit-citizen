c1c7773a0c110bba54847a3d22310822c99f9292
오윤지 <network_dude@naver.com>
2022-11-18 20:15:40 +0900

    feat: 프로필 레포지토리 추가

---
+++ repositories/profileRepositories.js
@@ -0,0 +1,65 @@
+const User = require('../schemas/user');
+
+class ProfileRepositories {
+
+    editProfileInfo_DB = async (
+        snsId,
+        nickname,
+        gender,
+        representProfile,
+        profileImage,
+        statusmessage
+    ) => {
+            try {
+                if( snsId ) {
+                    const editedProfileData = await User.findOneAndUpdate(
+                        { snsId },
+                        { 
+                            nickname, 
+                            gender, 
+                            representProfile, 
+                            profileImage, 
+                            statusmessage  
+                        }
+                    );
+                    return editedProfileData;
+                } else {
+                    throw new Error('snsId 값이 없으면 유저 프로필 정보를 수정할 수 없습니다');
+                }
+            } catch (error){
+                console.log(error.name);
+                console.log(error.message);
+            }
+        }
+
+    findProfileInfo_DB = async ( snsId ) => {
+        try {
+            if ( snsId ) {
+                const profileData = await User.findOne({ snsId });
+                return profileData;
+            } else {
+                throw new Error('snsId 값이 없으면 유저 프로필 정보를 찾을 수 없습니다');
+            }
+        } catch (error) {
+            console.log(error.name);
+            console.log(error.message);
+        }
+    }
+
+    deleteProfileInfo_DB = async ( snsId ) => {
+        try {
+            if ( snsId ) {
+                const deletedProfileData = await User.deleteOne({ snsId });
+                return deletedProfileData;
+            } else {
+                throw new Error('snsId 값이 없으면 유저 프로필 정보를 지울 수 없습니다')
+            }
+        } catch (error) {
+            console.log(error.name);
+            console.log(error.message);
+        }
+    }
+
+}
+
+module.exports = ProfileRepositories;
