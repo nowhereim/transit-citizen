708db151a506acf757e8977882817646827a9fa8
오윤지 <network_dude@naver.com>
2022-11-21 07:54:56 +0900

    Feat: 이미지 파일 업로드, 프로필 정보 수정, 프로필 정보 불러오기 서비스
    - 이미지 파일 업로드
    유저 정보가 존재할 떄 이미지파일 업로드 기능을 수행
    - 프로필 정보 수정
    유저 정보가 존재할 때 프로필 정보 수정 기능을 수행
    - 프로필 정보 불러오기
    유저 정보가 존재할 때 프로필 정보 불러오기 기능을 수행
    
    Related to: #11

--- services/profileServices.js
+++ services/profileServices.js
@@ -1,3 +1,6 @@
+const cloudinary = require("cloudinary");
+const DatauriParser = require('datauri/parser');
+const parser = new DatauriParser();
 const ProfileRepositories = require('../repositories/profileRepositories');
 
 class ProfileServices {
@@ -6,15 +9,12 @@ class ProfileServices {
         this.profileRepository = new ProfileRepositories();
     }
 
-    editUserProfileInfo = async ( snsId, nickname, representProfile, profileImage, statusmessage ) => {
+    editUserProfileInfo = async ( snsId, nickname, statusmessage ) => {
         try {
-
-            if ( snsId && nickname && representProfile && profileImage ) {
+            if ( snsId && nickname ) {
                 const dbProfileInfo = await this.profileRepository.editProfileInfo_DB(
                     snsId,
                     nickname,
-                    representProfile,
-                    profileImage,
                     statusmessage
                 );
                 return dbProfileInfo;
@@ -23,11 +23,75 @@ class ProfileServices {
                     ':: 필수 입력 필드 값은 반드시 모두 채워져야 합니다 :: profileServices.js ::'
                 );
             }
-            
         } catch (error) {
             console.log(error.name);
             console.log(error.message);
         }
+    }
+
+    // 이미지 업로드 서비스
+    editUserImageProfileInfo = async ( snsId, representProfile, profileImage ) => {
+        try {
+
+            if ( !snsId ) {
+                throw new Error('필수 필드 값이 비어있습니다 :: profileServices.js');
+            }
+
+            if ( !profileImage && !representProfile ) { 
+                // profileImage === [] && representProfile === null
+                const dbImageProfileInfo 
+                    = await this.profileRepository.findProfileInfo_DB( snsId );
+                return dbImageProfileInfo;
+            }
+
+            if ( representProfile || profileImage ) {
+
+                const representProfileBuffer = representProfile[0].buffer;
+                const refile = parser.format('.png', representProfileBuffer).content;
+                const urls = [];
+                var i = 0;
+
+                cloudinary.v2.uploader.upload( refile, async (error, result) => {
+                    if ( error ) {
+                        throw new Error('이미지 업로드 불가');
+                    };
+
+                    await this.profileRepository.editRepresentProfileInfo_DB( 
+                        snsId,
+                        result.url
+                    );
+
+                });
+                
+
+                await ( async function() {
+                    for (const file of profileImage) {
+                        i++
+                        const buffer = file.buffer;
+                        const refiles = parser.format('.png', buffer).content;
+
+                        await cloudinary.v2.uploader.upload( refiles, async (error, result) => {
+
+                            if(error) {
+                                return res.send(error.message);
+                            }
+                            urls.push( result.url );
+                        });
+                    }
+                })();
+                
+                const dbImageProfileInfo = await this.profileRepository.editProfileImageInfo_DB(
+                    snsId,
+                    urls
+                );
+
+                return dbImageProfileInfo;
+            }
+        } catch (error) {
+            console.log(error.name);
+            console.log(error.message);
+        }
+
     }
 
     getUserProfileInfo = async ( snsId ) => {
@@ -46,8 +110,6 @@ class ProfileServices {
             console.log(error.message);
         }
     }
-
-
 
 }
 
