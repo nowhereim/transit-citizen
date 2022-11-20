947339fba70aa1aa138e9277b248c37b9c3f8dea
오윤지 <network_dude@naver.com>
2022-11-20 22:03:24 +0900

    Add: 프로필 업데이트 라우터에 이미지 업로드 미들웨어 적용
    프로필 업데이트 라우터에 이미지 업로드 미들웨어 적용
    
    Related to: #11

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const imagesUploadMiddleware = require("../middlewares/imagesUploadMiddleware");
 const ProfileControllers = require('../controllers/profileControllers');
 this.profileControllers = new ProfileControllers();
 
 // 프로필 정보 변경
-router.post('/', this.profileControllers.editProfileInfo );
+router.post('/', imagesUploadMiddleware, this.profileControllers.editProfileInfo );
 
 // 프로필 정보 가져오기
 router.get('/', this.profileControllers.showProfileInfo );
