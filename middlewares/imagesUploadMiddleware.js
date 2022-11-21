34c97dc71b437aebbaaad01fb59ec35594d18d09
오윤지 <network_dude@naver.com>
2022-11-21 17:59:42 +0900

    Feat: 이미지 업로드 미들웨어 ( 대표 프로필 1장 )
    이미지 업로드 미들웨어 추가
    - 기존 : 대표 이미지 1장 + 추가 프로필 5장 받을 수 있는 멀터 미들웨어
    - 추가 : 가입시 대표 이미지 1장만 받을 수 있는 멀터 미들웨어
    
    Related to: #16

--- middlewares/imagesUploadMiddleware.js
+++ middlewares/imagesUploadMiddleware.js
@@ -11,3 +11,12 @@ module.exports = multer({
     { name : 'representProfile', maxCount: 1 },
     { name : 'profileImage', maxCount: 5 }
 ]);
+
+const representProfileUpload = multer({
+    storage,
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    }
+}).single('representProfile');
+
+module.exports = representProfileUpload; 
