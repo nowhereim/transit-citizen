6ebe2eeb1a0167253c9747f92043681598f53529
오윤지 <network_dude@naver.com>
2022-11-21 10:14:55 +0900

    Feat: 유저 인증 미들웨어 라우터에 장착

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const imagesUploadMiddleware = require("../middlewares/imagesUploadMiddleware");
+const authMiddleware = require('../middlewares/auth_middleware');
 const ProfileControllers = require('../controllers/profileControllers');
 this.profileControllers = new ProfileControllers();
 
 // 프로필 정보 변경
-router.post('/', imagesUploadMiddleware, this.profileControllers.editProfileInfo );
+router.post('/', authMiddleware, imagesUploadMiddleware, this.profileControllers.editProfileInfo );
 
 // 프로필 정보 가져오기
-router.get('/', this.profileControllers.showProfileInfo );
+router.get('/', authMiddleware, this.profileControllers.showProfileInfo );
 
 module.exports = router;
