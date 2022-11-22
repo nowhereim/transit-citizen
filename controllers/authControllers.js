32da7819c5683ca3103208635f4f91ff4fbdec7d
오윤지 <network_dude@naver.com>
2022-11-22 16:15:01 +0900

    Feat: 인증번호 검증 추가
    인증번호 검증 기능 추가
    
    Related to: #31

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -10,8 +10,28 @@ class AuthControllers {
         try {
             const { phoneNumber } = req.body;
             await this.authServices.sendAuthorityCheckMessage( phoneNumber );
-            return res.send({ msg: '인증 메시지를 전송했습니다' });
+            return res.send({ 
+                msg: '인증 메시지를 전송했습니다' 
+            });
         } catch (error) {
+            console.log(error);
+        }
+    }
+
+    compareAuthInputWithOurs = async (req, res) => {
+        try {
+            const { phoneNumber, authCode } = req.body;
+            if ( !phoneNumber || !authCode ) {
+                return res.status(400).send({ 
+                    error: '필수 입력 정보가 비어있습니다.'
+                });
+            }
+            const test = await this.authServices.checkAuthNumber( phoneNumber, authCode );
+            if ( test === null ) {
+                return res.status(400).send({ msg: '인증변호가 만료되었습니다'});
+            }
+            return res.status(200).send({ msg: '성공' });
+        } catch(error) {
             console.log(error);
         }
     }
