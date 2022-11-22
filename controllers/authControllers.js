4f4795c2c91f614a626fa4c2ef80bfa4856ab142
오윤지 <network_dude@naver.com>
2022-11-22 10:41:01 +0900

    Feat: auth 컨트롤러 폰 번호 받아서 메시지 인증 서비스로 넘겨주는 메서드 추가

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -6,26 +6,11 @@ class AuthControllers {
         this.authServices = new AuthServices;
     }
 
-    // 전화 번호 받아서 인증 번호 발송하는 메서드
     getUserPhoneNumber = async(req, res) => {
         try {
-            const { phone } = req.body;
-            await this.authServices.sendAuthorityCheckMessage(phone);
-            res.send('인증 메시지를 전송했습니다');
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    // 인증 번호 받아서 디비에 있는 값이랑 비교하는 메서드
-    compareAuthInputWithOurs = async(req, res) => {
-        try {
-            const { phone, auth } = req.body;
-            const test = await this.authServices.checkAuthNumber( phone, auth );
-            if(test === -1) {
-                res.status(400).send('없음');
-            }
-            res.status(400).send('성공');
+            const { phoneNumber } = req.body;
+            await this.authServices.sendAuthorityCheckMessage( phoneNumber );
+            return res.send({ msg: '인증 메시지를 전송했습니다' });
         } catch (error) {
             console.log(error);
         }
