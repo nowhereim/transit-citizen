c3c1d57bc67d9b77689ce1743d2f068ce770d167
AnTaeHwan <zyle1104@gmail.com>
2022-12-01 09:51:28 +0900

    repactor updateApp.js

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const AuthRoutes = require('./authRoutes');
-const UserRoutes = require('./userRoutes');
-const ProfileRoutes = require('./profileRoutes');
+const AuthRoutes = require("./authRoutes");
+const UserRoutes = require("./userRoutes");
+const ProfileRoutes = require("./profileRoutes");
 
+const kakaoRouter = require("./kakao.js");
+const testRouter = require("./test.js");
 
-const kakaoRouter = require('./kakao.js');
-const testRouter = require('./test.js');
-
-
-router.use('/auth', kakaoRouter);
-router.use('/test', testRouter);
+router.use("/auth", kakaoRouter);
+router.use("/test", testRouter);
 
 // 각종 인증 기능
-router.use('/auth2', AuthRoutes);
+router.use("/auth2", AuthRoutes);
 
 // 유저 정보
-router.use('/user', UserRoutes);
+router.use("/user", UserRoutes);
 
 // 프로필 정보
-router.use('/profile', ProfileRoutes);
+router.use("/profile", ProfileRoutes);
 
-
-module.exports=router;
+module.exports = router;
