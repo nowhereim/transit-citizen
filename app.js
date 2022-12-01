9e0d62188bc5e3145038556c02928443c2da8147
AnTaeHwan <zyle1104@gmail.com>
2022-12-01 09:49:07 +0900

    repactor updateApp.js

--- app.js
+++ app.js
@@ -46,12 +46,6 @@ app.use(
 app.get("/", (req, res) => {
   res.render("socket"); // socket.ejs
 });
-
-// app.use(helmet());
-// app.use(helmet.contentSecurityPolicy()); //contentSecurityPolicy의 역할은 바로 브라우저가 로딩하는 리소스를 제한하는 것이다. 이를 통해 XSS, 클릭재킹, 데이터 인젝션 등의 공격을 방어할 수 있다.
-// app.use(helmet.frameguard()); // frameguard의 역할은 X-Frame-Options 헤더를 설정하는 것이다. 이 헤더는 페이지가 iframe 안에 포함되는 것을 막아준다. 이는 클릭재킹 공격을 방지하는데 도움이 된다.
-// app.use(helmet.hsts()); //hsts의 역할은 Strict-Transport-Security 헤더를 설정하는 것이다. 이 헤더는 HTTPS를 사용하는 동안에만 웹사이트에 접근할 수 있도록 한다. 이는 중간자 공격을 방지하는데 도움이 된다.
-// app.use(helmet.xssFilter()); //xssFilter의 역할은 X-XSS-Protection 헤더를 설정하는 것이다. 이 헤더는 브라우저가 XSS 공격을 감지하고 방어하는 것을 허용하도록 한다.
 app.use("/", routes);
 
 app.get("/authbaby", authMiddleware, (req, res) => {
