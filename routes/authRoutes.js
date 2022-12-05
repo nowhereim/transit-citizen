2100276feed41f5932ce8ab11958ba2ed529c849
오윤지 <network_dude@naver.com>
2022-12-06 04:07:31 +0900

    Feat: 선언

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth_middleware");
 const AuthControllers = require("../controllers/authControllers");
 this.authControllers = new AuthControllers();
 
