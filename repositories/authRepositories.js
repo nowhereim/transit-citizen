48fbb0d2e6a1f43733004b8fb466b06d8730f4d0
오윤지 <network_dude@naver.com>
2022-12-06 02:00:53 +0900

    Feat: 문자 인증 로직 변경

--- repositories/authRepositories.js
+++ repositories/authRepositories.js
@@ -1,36 +1,34 @@
-const Auth = require('../schemas/auth');
+const Auth = require("../schemas/auth");
 
 class AuthRepositories {
-
-    findUserData = async ({ phoneNumber, authCode }) => {
-        try {
-            const data = await Auth.findOne({ phoneNumber,authCode });
-            return data;
-        } catch (error) {
-            throw Error('유저 정보 없음');
-        }
+  findUserData = async ({ snsId, phoneNumber, authCode }) => {
+    try {
+      const data = await Auth.findOne({ phoneNumber, authCode });
+      if (data !== null)
+        await User.findOneAndUpdate({ snsId }, { phoneNumber });
+      return data;
+    } catch (error) {
+      throw error;
     }
+  };
 
-    destroyUserData = async ( phoneNumber ) => {
-        try {
-            await Auth.deleteOne({ phoneNumber }); 
-            return;
-        } catch (error) {
-            throw error;
-        }
+  destroyUserData = async (phoneNumber) => {
+    try {
+      await Auth.deleteOne({ phoneNumber });
+      return;
+    } catch (error) {
+      throw error;
     }
+  };
 
-    pushPhoneAuthData = async ( phoneNumber, authCode ) => {
-        try {
-            await Auth.create( phoneNumber, authCode );
-            return;
-        } catch (error) {
-            throw error;
-        }
+  pushPhoneAuthData = async (phoneNumber, authCode) => {
+    try {
+      await Auth.create(phoneNumber, authCode);
+      return;
+    } catch (error) {
+      throw error;
     }
-
-    
-
+  };
 }
 
 module.exports = AuthRepositories;
