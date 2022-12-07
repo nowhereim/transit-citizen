1da1fe30b7bae3cc1fb96b743520e0a334cbacbf
오윤지 <network_dude@naver.com>
2022-12-07 20:22:57 +0900

    Fix : 에러 처리

--- controllers/profileControllers.js
+++ controllers/profileControllers.js
@@ -6,68 +6,55 @@ class ProfileControllers {
   }
 
   // 프로필 업데이트 기능
-  editProfileInfo = async (req, res) => {
+  editProfileInfo = async (req, res, next) => {
     try {
       const snsId = res.locals.user.user.snsId;
       const { nickname, statusmessage } = req.body;
+      if (!snsId) return res.status(400).send({ error: "알 수 없는 사용자" });
+      if (!nickname && !statusmessage && !req.files)
+        return res.status(200).send({ msg: "변경된 내용이 없습니다" });
+      if (!req.files)
+        return res.status(400).send({ error: "대표 프로필을 설정해주세요" });
+      if (!req.files.representProfile)
+        return res.status(400).send({ error: "대표 프로필을 설정해주세요" });
       const { representProfile, profileImage } = req.files;
-
-      if (!snsId) {
-        return res.status(400).send({
-          error: "알 수 없는 사용자",
-        });
-      }
-
-      if (!nickname && !statusmessage && !representProfile && !profileImage) {
-        return res.status(200).send({
-          msg: "변경된 내용이 없습니다",
-        });
-      }
-
       await this.profileServices.editUserProfileInfo(
         snsId,
         nickname,
-        statusmessage,
+        statusmessage
       );
-
       await this.profileServices.editUserImageProfileInfo(
         snsId,
         representProfile,
-        profileImage,
+        profileImage
       );
-
-      res.status(200).send({
-        status: 200,
-        msg: "유저 프로필 정보가 수정되었습니다",
-      });
+      return res
+        .status(200)
+        .send({ status: 200, msg: "유저 프로필 정보가 수정되었습니다" });
     } catch (error) {
-      console.log(error.name);
-      console.log(error.message);
+      next(error);
     }
   };
 
   // 프로필 조회
-  showProfileInfo = async (req, res) => {
+  showProfileInfo = async (req, res, next) => {
     try {
       const snsId = res.locals.user.user.snsId;
-
+      console.log(res.locals.user.user.snsId);
       if (!snsId) {
         return res.status(400).send({
           error: "유저 정보가 비어있습니다",
         });
       }
-
       const userProfileInfo = await this.profileServices.getUserProfileInfo(
-        snsId,
+        snsId
       );
-
       res.status(200).json({
         msg: "유저 프로필이 조회되었습니다",
         body: userProfileInfo,
       });
     } catch (error) {
-      console.log(error.name);
-      console.log(error.message);
+      next(error);
     }
   };
 }
