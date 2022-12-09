3a3a27edda7cf270ba15f91e48e9ba4a105eb86c
오윤지 <network_dude@naver.com>
2022-12-10 00:55:59 +0900

    Refactor: 리팩토링

--- services/authServices.js
+++ services/authServices.js
@@ -1,10 +1,12 @@
 const AuthRepositories = require("../repositories/authRepositories");
+const UserRepositories = require("../repositories/userRepositories");
 const crypto = require("crypto");
 const SensServices = require("./sensServices");
 
 class AuthServices {
   constructor() {
     this.authRepositories = new AuthRepositories();
+    this.userRepositories = new UserRepositories();
     this.sensServices = new SensServices();
   }
 
@@ -14,25 +16,33 @@ class AuthServices {
       const random = Math.floor(Math.random() * 1000000) + 1;
       const authNumber = random.toString().padStart(6, "0");
       const message = `:: 환승시민 :: 본인 확인을 위해 인증번호 [${authNumber}]를 입력해주세요`;
+      const data = await this.authRepositories.getSamePhoneNumber(phoneNumber);
+      if (data !== null)
+        throw new Error(
+          "이미 인증번호가 전송된 유저입니다. 3분 뒤 다시 시도해주세요"
+        );
       this.sensServices.send_message(phoneNumber, message);
       await this.authRepositories.pushPhoneAuthData({
         phoneNumber,
         authCode: authNumber,
       });
+      return;
     } catch (error) {
       throw error;
     }
   };
 
-  checkAuthNumber = async (phoneNumber, authCode) => {
+  checkAuthNumber = async (snsId, phoneNumber, authCode) => {
     try {
-      const data = await this.authRepositories.findUserData({
+      const empty = await this.authRepositories.findPhoneAuthData({
         phoneNumber,
         authCode,
       });
-      return data;
+      if (empty !== null)
+        await this.userRepositories.updatePhoneNumber(snsId, phoneNumber);
+      return empty;
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   };
 }
