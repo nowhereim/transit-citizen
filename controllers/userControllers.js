7b1bb9ee25e475da31d1ab9a158153bc989fac71
오윤지 <network_dude@naver.com>
2022-12-10 02:34:50 +0900

    Refactor: 전반적인 리팩토링

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,14 +4,12 @@ class userControllers {
   constructor() {
     this.userServices = new UserServices();
   }
-  // 로컬 회원가입
-  localSignUpInfo = async (req, res, next) => {
+
+  getLocalSignUpInfo = async (req, res, next) => {
     try {
       const { snsId, password, confirmpassword } = req.body;
       if (password.includes(snsId))
-        return res
-          .status(400)
-          .send({ error: "패스워드에 아이디 값 사용 불가" });
+        return res.status(400).send({ error: "패스워드에 아이디 값 사용 불가" });
       const userIdCheck = await this.userServices.checkIsSameUserId(snsId);
       if (userIdCheck === false)
         return res.status(400).send({ error: "중복된 아이디 입니다" });
@@ -28,16 +26,11 @@ class userControllers {
       const representProfile = req.file.buffer;
       const { nickname, gender } = req.body;
       if (!snsId)
-        return res
-          .status(400)
-          .send({ error: "아이디 값을 받아올 수 없습니다" });
-      const userNicknameCheck = await this.userServices.checkIsSameUser(
-        nickname
-      );
-      if (userNicknameCheck === false)
-        return res.status(400).send({ error: "중복된 닉네임 입니다" });
-      await this.userServices.getUserRequiredProfile(snsId, representProfile);
-      await this.userServices.createUserRequiredInfo(snsId, nickname, gender);
+        return res.status(400).send({ error: "아이디 값을 받아올 수 없습니다" });
+      const nicknameCheck = await this.userServices.checkIsSameNickname(nickname);
+      if (nicknameCheck === false)
+        return res.status(400).send({ isSameName: true, error: "중복된 닉네임 입니다" });
+      await this.userServices.createUserRequiredInfo(snsId, nickname, gender, representProfile);
       return res.status(200).send({
         msg: "유저 필수 정보가 입력되었습니다.",
         snsId: res.locals.user.user.snsId,
@@ -52,26 +45,23 @@ class userControllers {
   nicknameCheck = async (req, res) => {
     try {
       const { nickname } = req.body;
-      const userNicknameCheck = await this.userServices.checkIsSameUser(
-        nickname
-      );
-      if (userNicknameCheck === false)
-        return res.status(400).send({ error: "중복된 닉네임 입니다." });
-      return res.status(200).send({ msg: "사용 가능한 닉네임 입니다." });
+      const nicknameCheck = await this.userServices.checkIsSameNickname(nickname);
+      if (nicknameCheck === false)
+        return res.status(400).send({ isSameName: true, error: "중복된 닉네임 입니다." });
+      return res.status(200).send({ isSameName: false, msg: "사용 가능한 닉네임 입니다." });
     } catch (error) {
       next(error);
     }
   };
 
-  // 아이디 중복 확인
   userIdCheck = async (req, res, next) => {
     try {
       const { snsId } = req.body;
       if (!snsId) return res.status(400).send({ error: "아이디를 입력하세요" });
-      const userIdCheck = await this.userServices.checkIsSameUserId(snsId);
-      if (userIdCheck === false)
-        return res.status(400).send({ error: "중복된 아이디입니다." });
-      return res.status(200).send({ msg: "사용 가능한 아이디 입니다." });
+      const idCheck = await this.userServices.checkIsSameUserId(snsId);
+      if (idCheck === false)
+        return res.status(400).send({ isSameId: true, error: "중복된 아이디입니다." });
+      return res.status(200).send({ isSameId: false, msg: "사용 가능한 아이디 입니다." });
     } catch (error) {
       next(error);
     }
--- services/userServices.js
+++ services/userServices.js
@@ -17,65 +17,26 @@ class userServices {
 
   createLocalUserInfo = async (snsId, password) => {
     try {
-      console.log(snsId);
-      console.log("hello");
-      await this.userRepositories.createLocalUserInfo_DB(snsId, password);
+      await this.userRepositories.createLocalAccount(snsId, password);
       return;
     } catch (error) {
-      console.log(error.name);
-      console.log(error.message);
+      throw error;
     }
   };
 
-  createUserRequiredInfo = async (snsId, nickname, gender) => {
+  createUserRequiredInfo = async (snsId, nickname, gender, representProfile) => {
     try {
-      await this.userRepositories.createUserInfo_DB(snsId, nickname, gender);
-      return;
+      const file = parser.format(".png", representProfile).content;
+      await this.userRepositories.updateUserInfo(snsId, nickname, gender);
+      cloudinary.v2.uploader.upload(file, async (error, result) => {
+        if (error) throw new Error("이미지 업로드 불가");
+        await this.userRepositories.updateRProfile(snsId, result.url);
+      })
     } catch (error) {
-      console.log(error.name);
-      console.log(error.message);
+      throw error;
     }
-  };
-
-  getUserRequiredProfile = async (snsId, representProfile) => {
-    try {
-      if (snsId) {
-        const refile = parser.format(".png", representProfile).content;
-
-        cloudinary.v2.uploader.upload(refile, async (error, result) => {
-          if (error) {
-            throw new Error("이미지 업로드 불가");
-          }
-
-          const createdUserInfoData =
-            await this.userRepositories.createUserRequiredProfile_DB(
-              snsId,
-              result.url
-            );
-          return createdUserInfoData;
-        });
-      } else {
-        throw new Error("snsId 값이 없으면 유저 정보를 조회할 수 없습니다");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  checkIsSameUser = async (nickname) => {
-    try {
-      if (nickname) {
-        const data = await this.userRepositories.isSameUser_DB(nickname);
-        console.log(`service ${data}`);
-        if (data !== null) return false; // 닉네임 중복 O
-        return;
-      }
-    } catch (error) {
-      throw new Error("nickname이 정의되지 않았습니다");
-    }
-  };
-
-
+  }
+  
   login = async (snsId, password) => {
     const userInfo = await this.userRepositories.getUserInfo(snsId);
     // console.log("userInfo-->", userInfo);
@@ -87,13 +48,11 @@ class userServices {
     if (!same) {
       throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
     }
- 
-    const donePhoneNumber = (!userInfo.phoneNumber) ? false : true;
+
+    const donePhoneNumber = !userInfo.phoneNumber ? false : true;
 
     const doneAdditionalInfo =
-      !userInfo.nickname || !userInfo.representProfile
-        ? false
-        : true;
+      !userInfo.nickname || !userInfo.representProfile ? false : true;
 
     const tokenInfo = await this.tokenRepository.getTokenInfo(snsId);
 
@@ -133,9 +92,19 @@ class userServices {
 
   checkIsSameUserId = async (snsId) => {
     try {
-      const data = await this.userRepositories.isSameUserId_DB(snsId);
-      if (data !== null) return false; // 유저 아이디 중복 O
+      const data = await this.userRepositories.findById(snsId);
+      if (data !== null) return false; // 아이디 중복 O
       return;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  checkIsSameNickname = async (nickname) => {
+    try {
+        const data = await this.userRepositories.findByNickname(nickname);
+        if (data !== null) return false; // 닉네임 중복 O
+        return;
     } catch (error) {
       throw error;
     }
