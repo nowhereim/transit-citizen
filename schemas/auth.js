2b07b23d698c851c67160766cc29842c2822c63b
오윤지 <network_dude@naver.com>
2022-12-09 11:22:20 +0900

    FIX: 안쓰는 라이브러리 제거
    const moment = require("moment");
    require("moment-timezone");
    const now = moment().format("YYYY-MM-DD HH:mm:ss");
    부분 제거

--- schemas/auth.js
+++ schemas/auth.js
@@ -1,8 +1,5 @@
 var mongoose = require("mongoose");
 const { Schema } = mongoose;
-const moment = require("moment");
-require("moment-timezone");
-const now = moment().format("YYYY-MM-DD HH:mm:ss");
 
 const authSchema = new Schema(
   {
